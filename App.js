815951652ecab8da04cd7a922e8de5e9f91bd7c7
Oleg <oleg_terletsky@hotmail.com>
2018-04-22 09:29:31 -0400

    Test new button

--- App.js
+++ App.js
@@ -1,9 +1,59 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        //Write app code here
 
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+    launch: function() {
+        this.iterationCombobox = this.add({
+            xtype: 'rallyiterationcombobox',
+            listeners: {
+                ready: this._onIterationComboboxLoad,
+                select: this._onIterationComboboxChanged,
+                scope: this
+            }
+        });
+    },
+
+    _onIterationComboboxLoad: function() {
+        var addNewConfig = {
+            xtype: 'rallyaddnew',
+            recordTypes: ['User Story', 'Defect'],
+            ignoredRequiredFields: ['Name', 'ScheduleState', 'Project'],
+            showAddWithDetails: true,
+            listeners: {
+                create: function(addNew, record) {
+                       this.Msg.alert('Add New', 'Added record named ' + record.get('Name'));
+                       },
+                beforecreate: this._onBeforeCreate,
+                scope: this
+            }
+        };
+
+        this.addNew = this.add(addNewConfig);
+
+        var cardBoardConfig = {
+            xtype: 'rallycardboard',
+            context: this.getContext(),
+            types: ['Defect', 'User Story'],
+            attribute: 'ScheduleState',
+            storeConfig: {
+                filters: [this.iterationCombobox.getQueryFromSelected()]
+            }
+        };
+        this.cardBoard = this.add(cardBoardConfig);
+	//this.render();
+    },
+
+    _onBeforeCreate: function(addNewComponent, record) {
+        record.set('Iteration', this.iterationCombobox.getValue());
+    },
+
+    _onIterationComboboxChanged: function() {
+        var config = {
+            storeConfig: {
+                filters: [this.iterationCombobox.getQueryFromSelected()]
+            }
+        };
+
+        this.cardBoard.refresh(config);
     }
 });
